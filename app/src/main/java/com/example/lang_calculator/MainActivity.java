5249431a34bed5d2b220c8cb3fa9cd251842c238
Puten <afaputen@list.ru>
2019-04-15 23:36:29 +0300

    Revert "margins"
    This reverts commit b4397cf793de2f9b216a941deef5347936360c3f.

--- app/src/main/java/com/example/lang_calculator/MainActivity.java
+++ app/src/main/java/com/example/lang_calculator/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
 
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
